e60369aa84f5ca87a5f26c2a4320210a52826f2b
peicq521 <1970664567@qq.com>
2019-04-08 00:38:10 +0800

    app111

--- shop-vo/src/test/java/com/fh/shop/backend/App1.java
+++ shop-vo/src/test/java/com/fh/shop/backend/App1.java
@@ -3,5 +3,6 @@ package com.fh.shop.backend;
 public class App1 {
     public static void main(String[] args) {
         System.out.println("dadad");
+        System.out.println("ajlfsklas;fjk;adjf;lfdkasj;fkla;sf");
     }
 }
